1478b0bf22406d717c64c5b62faf52b5206f7311
ShaundaK <sdking1913@gmail.com>
2020-12-07 23:52:14 -0600

    initial commit, works

---
+++ year.py
@@ -0,0 +1,5 @@
+name = input("What is your name?  ")
+age = input("What is your age?  ")
+nage = int(age)
+year = (100 - nage) + 2020
+print("Hello", name.capitalize() + "! You will be 100 in year", year,)
