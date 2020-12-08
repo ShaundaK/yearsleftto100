bb332ca3730a2f647e4b0fb12151b54effd53a02
ShaundaK <sdking1913@gmail.com>
2020-12-07 23:57:55 -0600

    add goal

--- year.py
+++ year.py
@@ -1,5 +1,7 @@
+#create a program that asks the user to enter their name and age. Print out message addressed to them that tells them the year that they will turn 100 years old.
+#Then ask user for another number and print out that many copies of the previous message
 name = input("What is your name?  ")
 age = input("What is your age?  ")
 nage = int(age)
 year = (100 - nage) + 2020
-print("Hello", name.capitalize() + "! You will be 100 in year", year,)
+print("Hello, {}! You will be 100 in year {}!".format(name.capitalize(), year))
